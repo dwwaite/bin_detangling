df1587cd9ce8853efb53a97cacb1d92cb5a0f16d
David Waite <david.waite@auckland.ac.nz>
2019-03-19 14:14:36 +1300

    Added tSNE as a clustering algorithm prior to gapstat calculations.

--- gapstat_cluster.py
+++ gapstat_cluster.py
@@ -7,6 +7,10 @@
         5. Birch (distance between points; uneven clusters allowed)
 
     Find them at https://scikit-learn.org/stable/modules/clustering.html
+
+    Debug line:
+    python gapstat_cluster.py -t 2 --min 2 --max 10 -o mock_cluster.pca --convex --plot tests/kmer.input.chomp1500.tsv
+    python gapstat_cluster.py -t 2 --min 2 --max 10 -o mock_cluster.tsne --ordinate-esom --convex --plot tests/kmer.input.chomp1500.tsv
 '''
 # Basic imports
 import sys, os
@@ -21,7 +25,7 @@ import matplotlib.colors as cmap
 from scipy.spatial import ConvexHull
 from sklearn.cluster import Birch
 from sklearn.decomposition import PCA
-#from sklearn.datasets.samples_generator import make_blobs # Only for generating test data
+from sklearn.manifold import TSNE
 
 # Custom imports
 from gap_statistic import OptimalK
@@ -30,26 +34,26 @@ from scripts.OptionValidator import ValidateInteger
 
 def main():
 
-    parser = OptionParser()
-    usage = "usage: %prog [options] [feature table]"
+    usageString = "usage: %prog [options] [feature table]"
+    parser = OptionParser(usage=usageString)
 
     ''' Parse and validate options - simple/expected choices '''
     parser.add_option('-t', '--threads', help='Number of threads to use (Default = 1)', dest='threads')
     parser.add_option('--min', help='Minimum number of clusters to test (Default = 2)', dest='min', default=2)
     parser.add_option('--max', help='Maximum number of clusters to test (Default = 200)', dest='max', default=200)
-    parser.add_option('--force_cluster', help='Skip gap-stat evaluation and cluster into N clusters', dest='force_cluster', default=None)
+    parser.add_option('--force-cluster', help='Skip gap-stat evaluation and cluster into N clusters', dest='force_cluster', default=None)
     parser.add_option('--sep', help='Separator value to delimit columns (Default = [tab])', dest='sep', default='\t')
     parser.add_option('-o', '--output', help='Name stub for all output files (Default: Inherited from input table)', dest='output_file')
-    parser.add_option('--log_contigs', help='Create per-cluster files containing contig names (Default: False)', dest='log_contigs', action='store_true')
+    parser.add_option('--ordinate-esom', help='Use the tSNE algorithm to create ordination and reduce dimensionality of data (Default: None, PCA for plotting)', dest='ordinate_esom', action='store_true')
+    parser.add_option('--log-contigs', help='Create per-cluster files containing contig names (Default: False)', dest='log_contigs', action='store_true')
     parser.add_option('-p', '--plot', help='Plot the decision process (Default: False, but highly recommended)', dest='plot', action='store_true')
     parser.add_option('--convex', help='Plot convex hulls of the bin outlines. Useful for deciding whether or not to merge bins (Default: False)', dest='convex', action='store_true')
-
 
     options, arguments = parser.parse_args()
 
     ''' Parse the options and arguments '''
     featureTableName = arguments[0]
-    featureTable = ParseFeatureTable(featureTableName, options.sep)
+    featureTable = pd.read_csv(featureTableName, sep=options.sep)
 
     options.output_file = options.output_file if options.output_file else os.path.splitext(featureTableName)[0]
 
@@ -58,62 +62,69 @@ def main():
     options.threads = ValidateInteger(options.threads, 'number of threads', behaviour='default', defaultValue=1)
     options.force_cluster = ValidateInteger(options.force_cluster, 'user-defined number of clusters', behaviour='skip', defaultValue=None)
 
-    ''' Determine clustering size, or skip if using --force_cluster. Append clustering information to DataFrame '''
+    ''' Determine clustering size, or skip if using --force_cluster. Append clustering information to DataFrame.
+        If using ESOM to ordinate data, transform the table at this stage. '''
+
+    contigNames = featureTable.pop('Contig')
+
+    if options.ordinate_esom:
+        featureTable = ReduceViaEsom(featureTable)
+
+    else:
+        featureTable = featureTable.values
+        
     cEngine, n_clusters = OptmiseClustering(featureTable, options.threads, options.min, options.max, options.force_cluster)
-    mappedFeatureTable = ApplyClustering(featureTable, n_clusters)
+    clusterIdentities = ApplyClustering(featureTable, n_clusters)
 
     '''' Log statistics, and plot if desired - skip if using force_cluster '''
     if not options.force_cluster:
         LogGapStatistics(cEngine, options.output_file)
 
-    ''' If request, plot ordination of points with cluster information '''
-    plotPCA = ReduceToPCA(mappedFeatureTable)
-    colourLookup = MapColourSpace( mappedFeatureTable.Cluster )
+    ''' If requested, plot ordination of points with cluster information '''
+    plotObj = EsomToPlot(featureTable, contigNames, clusterIdentities) if options.ordinate_esom else ReduceToPCA(featureTable, contigNames, clusterIdentities)
+    colourLookup = MapColourSpace(clusterIdentities)
 
-    if options.plot:
+    ''' Plot the outputs, if desired.
+        Not the most simple method in terms of number of statements evaluated, but makes the logic simple to observe '''
 
-        PlotClusters(plotPCA, colourLookup, options.output_file)
 
-        ''' Plot the clustering decision process, if requested '''
-        if not options.force_cluster: PlotResults(cEngine, n_clusters, options.output_file)
-
-    if options.convex: PlotConvexHulls(plotPCA, colourLookup, options.output_file)
+    if options.plot: PlotClusters(plotObj, colourLookup, options.output_file)
+    if options.plot and options.convex: PlotConvexHulls(plotObj, colourLookup, options.output_file)
+    
+    if options.plot and not options.force_cluster: PlotResults(cEngine, n_clusters, options.output_file)
 
     ''' Write out the results as a table with the ordination coordinates and cluster identifiers, then as accnos files for each bin if requested '''
-    WriteOutputFiles(plotPCA.df, options.output_file, options.log_contigs)
+    WriteOutputFiles(plotObj.df, options.output_file, options.log_contigs)
 
 ###############################################################################
 
 # region Clustering functions
 
+def ReduceViaEsom(df):
+
+    ''' Use a 50-dimension PCA as the starting place for tSNE clustering '''
+    pcaOrd = PCA(n_components=50).fit_transform(df.values)
+    tsneOd = TSNE(n_components=2, method='barnes_hut').fit_transform(pcaOrd)
+    return tsneOd
+
 def OptmiseClustering(featureTable, nThreads, minSize, maxSize, forceOpt):
 
-    if forceOpt:
-        return None, forceOpt
+    if forceOpt: return None, forceOpt
 
     ''' Instantiate the engine according to thread requirement '''
     clustEngine = OptimalK(parallel_backend='multiprocessing', n_jobs=nThreads) if nThreads > 1 else OptimalK(parallel_backend='None')
 
-    _, featureFrame = ExtractColumn(featureTable, 'Contig')
-
     ''' Where multiple best-case instances are reported, the default behaviour is to return the higher number of clusters '''
-    nClusters = clustEngine(featureFrame.values, cluster_array=np.arange(minSize, maxSize))
+    nClusters = clustEngine(featureTable, cluster_array=np.arange(minSize, maxSize))
     return clustEngine, nClusters
 
 def ApplyClustering(featureTable, n_clusters):
 
     ''' Perform the actual clustering '''
-    _, featureFrame = ExtractColumn(featureTable, 'Contig')
-    clustObj = Birch(n_clusters=n_clusters).fit( featureFrame.values )
+    clustObj = Birch(n_clusters=n_clusters).fit( featureTable )
 
-    ''' Append results to the DataFrame, then return it '''
-    featureTable = _bindClustering(featureTable, clustObj)
-
-    return featureTable
-
-def _bindClustering(dTable, clustObj):
-    dTable['Cluster'] = [ 'Cluster_{}'.format(b+1) for b in clustObj.labels_ ]
-    return dTable
+    ''' Create a vector of the clustering information and return it '''
+    return [ 'Cluster_{}'.format(b+1) for b in clustObj.labels_ ]
 
 def MapColourSpace(clusterColumn):
     clusterNames = list( set(clusterColumn) )
@@ -122,38 +133,34 @@ def MapColourSpace(clusterColumn):
 
 # endregion
 
-# region DataFrame manipulation
-
-def ParseFeatureTable(pFile, _sep):
-    return pd.read_csv(pFile, sep=_sep)
-
-def ExtractColumn(dTable, colName):
-    colValues = dTable.loc[:,[colName]]
-    dfValues = dTable.drop(colName, axis=1)
-    return colValues, dfValues
-
-# endregion
-
 # region Plotting functions
 
-def ReduceToPCA(mTable):
+def EsomToPlot(eTable, contigVector, clusterVector):
 
-    ''' Split out the contigs, then fit a 2D PCA '''
-    contigNames, dFrameCluster = ExtractColumn(mTable, 'Contig')
-    clusterNames, dFrame = ExtractColumn(dFrameCluster, 'Cluster')
-    dArray = dFrame.values
+    dESOM = namedtuple('plotObj', ['df', 'x', 'y'])
+    dESOM.df = pd.DataFrame(eTable, columns=['X', 'Y'])
 
+    dESOM.x = 'Axis 1'; dESOM.y = 'Axis 2'
+
+    dESOM.df['Contig'] = contigVector
+    dESOM.df['Cluster'] = clusterVector
+
+    return dESOM
+
+def ReduceToPCA(mTable, contigVector, clusterVector):
+
+    ''' Fit a 2D PCA '''
     pcaObj = PCA(n_components=2)
-    pcCordinates = pcaObj.fit_transform(dArray)
+    pcCoordinates = pcaObj.fit_transform(mTable)
 
     ''' Package the results into a namedtuple '''
-    dPCA = namedtuple('dPCA', ['df', 'pc1_label', 'pc2_label'])
-    dPCA.df = pd.DataFrame(data=pcCordinates, columns=['PC1', 'PC2'])
-    dPCA.df.insert(loc=0, column='Contig', value=mTable.Contig)
-    #dPCA.df['Contig'] = mTable.Contig
-    dPCA.df['Cluster'] = mTable.Cluster
-    dPCA.pc1_label = 'Principal Component 1 ({:.2f}% variance)'.format( pcaObj.explained_variance_ratio_[0] * 100 )
-    dPCA.pc2_label = 'Principal Component 2 ({:.2f}% variance)'.format( pcaObj.explained_variance_ratio_[1] * 100 ) 
+    dPCA = namedtuple('plotObj', ['df', 'x', 'y'])
+    dPCA.df = pd.DataFrame(data=pcCoordinates, columns=['X', 'Y'])
+    dPCA.df['Contig'] = contigVector
+    dPCA.df['Cluster'] = clusterVector
+
+    dPCA.x = 'Principal Component 1 ({:.2f}% variance)'.format( pcaObj.explained_variance_ratio_[0] * 100 )
+    dPCA.y = 'Principal Component 2 ({:.2f}% variance)'.format( pcaObj.explained_variance_ratio_[1] * 100 ) 
 
     return dPCA
 
@@ -170,57 +177,52 @@ def PlotResults(gapObj, n_clusters, outputName):
     ''' Tidy it up a bit, then save the figures '''
     _produceAndSavePlot(plt, 'Number of bins', 'Gap Value', outputName, 'gap_stat')
 
-def PlotClusters(pcaObj, colourLookup, outputName):
+def PlotClusters(plotObj, colourLookup, outputName):
 
     ''' Clear the plotting space '''
     plt.clf()
 
     ''' Plot the points '''
-    colValues = [ colourLookup[x] for x in pcaObj.df.Cluster ]
-    plt.scatter(pcaObj.df.PC1, pcaObj.df.PC2, c=colValues)
+    colValues = [ colourLookup[x] for x in plotObj.df.Cluster ]
+    plt.scatter(plotObj.df.X, plotObj.df.Y, c=colValues)
 
     ''' For each cluster, add a centroid label '''
-    clusterNames = _grabClusterNames(pcaObj.df)
-    for clusterName in clusterNames:
+    for clusterName in sorted( plotObj.df.Cluster.unique() ):
 
-        tempdf = pcaObj.df[ pcaObj.df.Cluster == clusterName ]
+        tempdf = plotObj.df[ plotObj.df.Cluster == clusterName ]
         x, y = _returnCentroid(tempdf)
         plt.text(x, y, clusterName, fontsize=20)
 
-    _produceAndSavePlot(plt, pcaObj.pc1_label, pcaObj.pc2_label, outputName, 'cluster')
+    _produceAndSavePlot(plt, plotObj.x, plotObj.y, outputName, 'cluster')
 
-def PlotConvexHulls(pcaObj, colourLookup, outputName):
+def PlotConvexHulls(plotObj, colourLookup, outputName):
 
     ''' Clear the plotting space '''
     plt.clf()
 
     ''' Plot the data '''
-    clusterNames = _grabClusterNames(pcaObj.df)
-    for clusterName in clusterNames:
+    for clusterName in sorted( plotObj.df.Cluster.unique() ):
 
-        tempdf = pcaObj.df[ pcaObj.df.Cluster == clusterName ]
+        tempdf = plotObj.df[ plotObj.df.Cluster == clusterName ]
         nRow, nCol = tempdf.shape
 
         ''' Can only do a ConvexHull if there are at least 3 points '''
         if nRow >= 3:
 
-            hull = ConvexHull( tempdf.loc[ : , ['PC1', 'PC2'] ].values )
+            hull = ConvexHull( tempdf.loc[ : , ['X', 'Y'] ].values )
             vDF = tempdf.iloc[ hull.vertices , : ]
 
-            plt.fill( vDF.PC1, vDF.PC2, c=colourLookup[clusterName], alpha=0.5 )
+            plt.fill( vDF.X, vDF.Y, c=colourLookup[clusterName], alpha=0.5 )
             x, y = _returnCentroid(tempdf)
             plt.text(x, y, clusterName.replace('Cluster_', ''), fontsize=10)
 
         else:
             print( 'Unable to compute spatial hull for cluster {} (requires 3 points, cluster contains {})'.format(clusterName, nRow) )
 
-    _produceAndSavePlot(plt, pcaObj.pc1_label, pcaObj.pc2_label, outputName, 'convex')
-
-def _grabClusterNames(pointDf):
-    return sorted( pointDf.Cluster.unique() )
+    _produceAndSavePlot(plt, plotObj.x, plotObj.y, outputName, 'convex')
 
 def _returnCentroid(df):
-    return ( np.median(df.PC1), np.median(df.PC2) )
+    return ( np.median(df.X), np.median(df.Y) )
 
 def _produceAndSavePlot(_plt, xLabel, yLabel, outputName, suffix):
     _plt.grid(True)
