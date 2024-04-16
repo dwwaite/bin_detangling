700ddcd69208f6e15e3d48328fca2114a9651629
David Waite <d.waite1986@gmail.com>
2024-04-16 20:46:29 +1200

    Added depth calculation

---
+++ bin/compute_depth_profile.py
@@ -0,0 +1,44 @@
+from optparse import OptionParser
+from typing import Generator, List, Tuple
+
+import polars as pl
+
+def main():
+
+    # Set up the options
+    parser = OptionParser()
+    parser.add_option('-o', '--output', help='Output file for depth profile results', dest='output')
+    options, input_files = parser.parse_args()
+
+    # Process the data
+    depth_frames = [parse_depth_file(file_path, depth_label) for file_path, depth_label in depth_label_generator(input_files)]
+    pl.concat(depth_frames, how='vertical').write_parquet(options.output)
+
+def depth_label_generator(file_paths: List[str]) -> Generator[Tuple[str, str], None, None]:
+    """ Creates a generator returning tuples of the file path and depth label for each file
+        in the input sequence. 
+    """
+    for i, file_path in enumerate(file_paths):
+        yield (file_path, f"Depth_{i+1}")
+
+def parse_depth_file(file_path: str, depth_label: str) -> pl.DataFrame:
+    """ Reads and summarises a headerless mapping file produced by samtools. Returns a DataFrame with
+        the columns `Contig`, `Coverage`, and `Label`.
+        Reports median depth over each contig to allow some weighting to the linkage across contig
+        fragments produced under compute_kmer_profile.py.
+    """
+    return (
+        pl
+        .scan_csv(file_path, separator='\t', has_header=False, new_columns=['Contig', 'pos', 'depth'])
+        .group_by(['Contig'])
+        .agg(
+            pl.col('depth').median().alias('Coverage')
+        )
+        .with_columns(
+            Label=pl.lit(depth_label)
+        )
+        .collect()
+    )
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_computer_depth_profile.py
@@ -0,0 +1,37 @@
+import unittest
+
+import polars as pl
+from polars.testing import assert_frame_equal
+
+from bin.compute_depth_profile import depth_label_generator
+from bin.compute_depth_profile import parse_depth_file
+
+class TestComputeDepthProfile(unittest.TestCase):
+
+    def test_depth_label_generator(self):
+
+        exp_result = [
+            ('abc.txt', 'Depth_1'),
+            ('def.txt', 'Depth_2'),
+            ('ghi.txt', 'Depth_3')
+        ]
+
+        obs_result = [x for x in depth_label_generator(['abc.txt', 'def.txt', 'ghi.txt'])]
+        self.assertListEqual(exp_result, obs_result)
+
+    def test_parse_depth_file(self):
+
+        exp_df = pl.DataFrame([
+            pl.Series('Contig', ['a', 'b', 'c']),
+            pl.Series('Coverage', [5, 4, 10.5]),
+            pl.Series('Label', ['Depth_1', 'Depth_1', 'Depth_1'])
+        ])
+
+        obs_df = parse_depth_file('tests/mock_depth.txt', 'Depth_1')
+        sorted_df = obs_df.sort('Contig', descending=False)
+
+        assert_frame_equal(exp_df, sorted_df)
+
+if __name__ == '__main__':
+
+    unittest.main()
